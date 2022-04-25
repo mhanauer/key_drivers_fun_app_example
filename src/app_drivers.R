137e17b9292709ff86e3ae2880a5a70c1b8893c6
mhanauer <mhanauer@indiana.edu>
2022-04-25 17:17:46 -0400

    Making sure RShiny is explicity installed.

--- src/app_drivers.R
+++ src/app_drivers.R
@@ -1,4 +1,4 @@
-
+install.packages('shiny', repos='http://cran.rstudio.com/')
 library(pacman)
 pacman::p_load(renv, dplyr, shiny, shinydashboard, shinydashboardPlus, tidyverse, glue, plotly, devtools, shinyWidgets, fontawesome, formattable, janitor, tsibble, DT, readr, lubridate, tidymodels, ggrepel)
 
